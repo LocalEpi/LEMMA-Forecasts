e2fa3c473dfc6e60f70d1765c93b409285c3db6d
joshuaschwab <jschwab77@berkeley.edu>
2020-07-05 08:29:19 -0700

    Data through 2020-07-03

--- Code/RunCAcounties.R
+++ Code/RunCAcounties.R
@@ -24,7 +24,8 @@ RunOneCounty <- function(county1, county.dt, county.pop, quick.test) {
   county.dt1[, cum.admits.conf := NA_integer_]
   county.dt1[, cum.admits.pui := NA_integer_]
   if (county1 == "Los Angeles") {
-    sheets$Interventions[2, mu_beta_inter := 0.5] #LA needs more informative prior to converge
+    sheets$Interventions <- sheets$Interventions[2:.N]
+    sheets$Interventions[1, mu_beta_inter := 0.5] #LA needs more informative prior to converge
     sheets$`Parameters with Distributions`[9, Mean := 0.64] #mort/ICU seems very high in LA
     sheets$`Parameters with Distributions`[9, `Standard Deviation` := 0.1]
   } else if (county1 == "Imperial") {
@@ -38,6 +39,8 @@ RunOneCounty <- function(county1, county.dt, county.pop, quick.test) {
   } else if (county1 == "San Mateo") {
     sheets$Interventions[7, mu_beta_inter := 1] #very recent increase
     sheets$Interventions[8, mu_beta_inter := 1.5]
+  } else if (county1 %in% c("Yolo", "Yuba", "El Dorado", "Lake", "Tuolumne")) {
+    # county.dt1 <- county.dt1[date >= as.Date("2020/5/1")] #noisy data early on
   }
 
   sheets$Data <- county.dt1
@@ -53,16 +56,36 @@ RunOneCounty <- function(county1, county.dt, county.pop, quick.test) {
   inputs$internal.args$lambda_ini_exposed <- 1 / mean.ini
 
   cred.int <- LEMMA:::CredibilityInterval(inputs)
+
+  max.date <- max(cred.int$inputs$obs.data$date)
+  outfile <- paste0("Scenarios/", county1)
+  cred.int$inputs$model.inputs$end.date <- as.Date("2020/12/31")
+
+  ProjScen <- function(int.date) {
+    if (is.na(int.date)) {
+      intervention <- NULL
+      subtitl <- "Scenario: No change from current Re"
+    } else {
+      intervention <- data.frame(mu_t_inter = int.date, sigma_t_inter = 0.0001, mu_beta_inter = 0.5, sigma_beta_inter = 0.0001, mu_len_inter = 7, sigma_len_inter = 2)
+      subtitl <- paste("Scenario: Reduce Re by 50% starting", as.character(as.Date(int.date), format = "%b%e"))
+    }
+    lapply(LEMMA:::ProjectScenario(cred.int, new.int=intervention, "Logs/temp")$gplot$long.term, function (z) z + ggplot2::labs(subtitle = subtitl))
+  }
+
+  scen.plots <- lapply(list(NA, max.date + 3, max.date + 17), ProjScen)
+  grDevices::pdf(file = paste0("Scenarios/", county1, "_scenarios.pdf"), width = 9.350, height = 7.225)
+  print(scen.plots)
+  dev.off()
+
   sink()
   ParallelLogger::logInfo("county = ", county1)
   return(cred.int)
 }
 
-
 county.dt <- GetCountyData(exclude.set)
 county.set <- unique(county.dt$county)
 
-if (quick.test) county.set <- c("San Mateo", "Kings", "Los Angeles")
+if (quick.test) county.set <- c("Butte",  "Kern")
 
 county.pop <- fread("Inputs/county population.csv")
 
