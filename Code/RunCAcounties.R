24074350a5a2c45357c8c29a6205d241b68b2b6f
joshuaschwab <jschwab77@berkeley.edu>
2020-09-28 14:40:09 -0700

    Data through 2020-09-26

--- Code/RunCAcounties.R
+++ Code/RunCAcounties.R
@@ -79,7 +79,7 @@ RunOneCounty <- function(county1, county.dt, county.pop, quick.test) {
     } else if (county1 %in% c("Kings", "San Joaquin", "Fresno", "Kern", "San Bernardino", "Butte")) {
       inputs$internal.args$adapt_delta <- 0.95
       inputs$internal.args$iter <- 1500 #needs more iterations to converge
-    } else if (county1 %in% c("Stanislaus", "Merced")) {
+    } else if (county1 %in% c("Stanislaus", "Merced", "Tulare")) {
       inputs$internal.args$adapt_delta <- 0.8
       inputs$internal.args$iter <- 1500 #needs more iterations to converge
     }
@@ -131,7 +131,7 @@ RunOneCounty <- function(county1, county.dt, county.pop, quick.test) {
 county.dt <- GetCountyData(exclude.set)
 county.set <- unique(county.dt$county)
 
-if (quick.test) county.set <- c("Santa Clara", "Santa Cruz", "Solano", "Stanislaus", "Tulare", "Yolo")
+if (quick.test) county.set <- c("Tulare")
 print(county.set)
 
 options(warn = 1)
