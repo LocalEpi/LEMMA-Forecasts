cd1ea1d84ea7434ff59e3de34f5b0296e321d0e2
joshuaschwab <jschwab77@berkeley.edu>
2020-11-04 06:31:27 -0800

    Data through 2020-11-01 partial

--- Code/RunCAcounties.R
+++ Code/RunCAcounties.R
@@ -3,7 +3,7 @@ library(ParallelLogger)
 
 source('Code/GetCountyData.R')
 
-quick.test <- F
+quick.test <- T
 if (quick.test) {
   cat("\n\n++++++++++++++++++  quick.test = T +++++++++++++++++ \n\n")
 }
@@ -130,7 +130,9 @@ RunOneCounty <- function(county1, county.dt, county.pop, quick.test) {
   county.dt <- GetCountyData(exclude.set)
   county.set <- unique(county.dt$county)
 
-  if (quick.test) county.set <- "San Mateo"
+  if (quick.test) county.set <- c("Santa Barbara",
+                                  "Sonoma", "Tehama", "Tuolumne", "Ventura",
+                                  "Yuba")
   print(county.set)
 
   options(warn = 1)
