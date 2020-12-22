7404362ba0c2b8eca20481b4cc85beb4c01ca1c8
joshuaschwab <jschwab77@berkeley.edu>
2020-12-21 22:08:20 -0800

    Santa Clara data through 2020-12-21

--- Code/RunCAcounties.R
+++ Code/RunCAcounties.R
@@ -9,12 +9,10 @@ exclude.set <- c("San Francisco") #SF is run separately
 county.dt <- GetCountyData(exclude.set)
 saveRDS(county.dt, "Inputs/CountyData.rds")
 
-quick.test <- F
+quick.test <- T
 if (quick.test) {
   cat("\n\n++++++++++++++++++  quick.test = T +++++++++++++++++ \n\n")
-  county.set <- c("Santa Clara", "Colusa", "Yuba",  "Mono", "San Luis Obispo",
-                  "Tehama", "Mendocino", "Madera", "Yolo", "Nevada", "Calaveras",
-                  "Humboldt", "El Dorado", "San Benito", "Amador", "Siskiyou")
+  county.set <- c("Santa Clara")
 } else {
   #order by last Rt date in forecasts and then last run time
   max.date <- county.dt[, max(date)]
