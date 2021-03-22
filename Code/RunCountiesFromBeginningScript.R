12a3cb2ed2dbf6ac218ac7225017d942971c0bd4
joshuaschwab <jschwab77@berkeley.edu>
2021-03-22 10:47:36 -0700

    data through 2021-03-21

--- Code/RunCountiesFromBeginningScript.R
+++ Code/RunCountiesFromBeginningScript.R
@@ -4,9 +4,10 @@ source('Code/GetCountyData.R')
 source('Code/RunCountiesFromBeginning.R')
 
 county.dt <- GetCountyData()
-saveRDS(county.dt, "~/Documents/Temp/savedCountyData.rds") #save in case HHS server is down later
 max.date <- Get1(county.dt[!is.na(hosp.conf), max(date), by = "county"]$V1)
 cat("max date = ", as.character(max.date), "\n")
+
+saveRDS(county.dt, "~/Documents/Temp/savedCountyData.rds") #save in case HHS server is down later
 doses.dt <- GetDosesData()
 
 county.by.pop <- unique(county.dt[!is.na(population), .(county, population)]) #NA population if no hospitalizations
