735be8fc7b2e7d5b19efdb57f5b91b868b16d135
joshuaschwab <jschwab77@berkeley.edu>
2021-03-25 09:41:59 -0700

    fix Del Norte, Yuba

--- Code/RunCountiesFromBeginning.R
+++ Code/RunCountiesFromBeginning.R
@@ -32,7 +32,7 @@ GetCountySheets <- function(county1, county.dt, doses.dt) {
     # sheets$Data <- merge(sheets$Data, sf.sheets$Data[, .(date, hosp.conf, hosp.pui, icu.conf, icu.pui)], by = "date", all = T)
 
     #add UeS cases
-    ues <- c(34, 39, 43, 45, 0, 50, 0, 57, 53, 44, 36, 0, 37, 0, 35, 43, 31, 23, 0, 38, 0, 0, 0, 0, 20, 0, 0, 0, 14, 16, 11, 19, 0, 0, 0, 12, 9, 10, 7, 0, 0, 0, 7, 12, 4, 7, 0, 0, 0, 5, 9, 2, 7, 0, 0, 0, 5, 1, 1, 2, 0, 0, 0, 7, 7, 3, 3, 0, 0, 0, 3, 1, 1)
+    ues <- c(34, 39, 43, 45, 0, 50, 0, 57, 53, 44, 36, 0, 37, 0, 35, 43, 31, 23, 0, 38, 0, 0, 0, 0, 20, 0, 0, 0, 14, 16, 11, 19, 0, 0, 0, 12, 9, 10, 7, 0, 0, 0, 7, 12, 4, 7, 0, 0, 0, 5, 9, 2, 7, 0, 0, 0, 5, 1, 1, 2, 0, 0, 0, 7, 7, 3, 3, 0, 0, 0, 3, 1, 1, 2)
     ues.dt <- data.table(date = as.Date("2021/1/10") + (1:length(ues)) - 1, ues)
     print(tail(ues.dt))
     sheets$Data <- merge(sheets$Data, ues.dt, by = "date", all.x = T)
@@ -98,6 +98,9 @@ GetCountyInputs <- function(county1, county.dt, doses.dt) {
     inputs$internal.args$init_frac_mort_nonhosp <- 0.001
   }
   if (county1 == "El Dorado") {
+    inputs$internal.args$init_frac_mort_nonhosp <- 0.001
+  }
+  if (county1 == "Del Norte") {
     inputs$internal.args$init_frac_mort_nonhosp <- 0.001
   }
 
