fb97a509c4010790b40c7e9f0fec917df217dce2
joshuaschwab <jschwab77@berkeley.edu>
2021-06-21 09:39:23 -0700

    data through 2021-06-20

--- LEMMA.forecasts/R/RunCounty_Scenario.R
+++ LEMMA.forecasts/R/RunCounty_Scenario.R
@@ -93,7 +93,7 @@ RunOneCounty_scen <- function(county1, county.dt, doses.dt, remote = FALSE, writ
     cat("reopenInc20: 20% increase in effective contact rate on June 15\n")
     cat("uptake85: 85% vaccine uptake in 12-64\n")
     cat("uptakeCurrent: 82% vaccine uptake in 12-64\n")
-    cat("All scenarios: vaccine uptake 92% in 65+; age 0-11 eligible Jan 1; Delta 30% today and dominant by July\n")
+    cat("All scenarios: vaccine uptake 92% in 65+; age 0-11 eligible Jan 1; Delta 30% on June 15 and dominant by July\n")
     cat("rel.cont.rate.cur = relative effective rate today\n")
     cat("rel.cont.rate.new = relative effective rate after reopening\n")
     cat("links to pdf and xlsx outputs: https://localepi.github.io/LEMMA/ \n")
